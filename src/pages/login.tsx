ef2832ae556f002de2491461bd794bfd33d8f081
hunman89 <hunman89@gmail.com>
2021-10-03 15:41:01 +0900

    add. login mutation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,24 @@
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
 
+const LOGIN_MUTAION = gql`
+  mutaion PotatoMutation($email:String!, $password:String!){
+    login(input: {
+      email:$email,
+      password:$password
+    }) {
+      ok
+      token
+      error
+    }
+  }
+`;
+
 interface ILoginForm {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 }
 
 export const Login = () => {
@@ -14,7 +28,16 @@ export const Login = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<ILoginForm>();
-  const onSubmit = () => {};
+  const [loginMutation, { loading, error, data }] = useMutation(LOGIN_MUTAION);
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    loginMutation({
+      variables: {
+        email,
+        password,
+      },
+    });
+  };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
       <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
@@ -35,7 +58,6 @@ export const Login = () => {
           <input
             {...register("password", {
               required: "Password is required",
-              minLength: 10,
             })}
             type="password"
             placeholder="Password"
