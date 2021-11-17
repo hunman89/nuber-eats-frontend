56d369cb43f72bb33265a12e00e05b3ec9789978
hunman89 <hunman89@gmail.com>
2021-11-17 22:22:25 +0900

    add. css in restaurant page header

--- src/pages/client/reataurant.tsx
+++ src/pages/client/reataurant.tsx
@@ -37,5 +37,26 @@ export const Restaurant = () => {
     }
   );
   console.log(data);
-  return <h1>Restaurant</h1>;
+  return (
+    <div>
+      <div
+        className="bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-4 pl-48">
+          <h4 className=" text-4xl mb-3">
+            {data?.restaurant.restaurant?.name}
+          </h4>
+          <h5 className="test-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="test-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
 };
