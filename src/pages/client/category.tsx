f50d06f60abdd69a57982bf85bb758dc955f5a09
hunman89 <hunman89@gmail.com>
2021-11-17 21:37:27 +0900

    add. css in category page

--- src/pages/client/category.tsx
+++ src/pages/client/category.tsx
@@ -1,11 +1,20 @@
 import { gql, useQuery } from "@apollo/client";
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { Link, useParams } from "react-router-dom";
+import { Restaurant } from "../../components/restaurant";
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import { category, categoryVariables } from "../../__generated__/category";
 
 const CATEGORY_QUERY = gql`
   query category($input: CategoryInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        ...CategoryParts
+      }
+    }
     category(input: $input) {
       ok
       error
@@ -28,18 +37,86 @@ interface ICategoryParams {
 }
 
 export const Category = () => {
+  const [page, setPage] = useState(1);
+  const onNextPageClick = () => setPage((current) => current + 1);
+  const onPrevPageClick = () => setPage((current) => current - 1);
   const param = useParams<ICategoryParams>();
   const { data, loading } = useQuery<category, categoryVariables>(
     CATEGORY_QUERY,
     {
       variables: {
         input: {
-          page: 1,
+          page,
           slug: param.slug,
         },
       },
     }
   );
-  console.log(data);
-  return <h1>Category</h1>;
+  return (
+    <div>
+      <Helmet>
+        <title>{param.slug} | Nuber Eats</title>
+      </Helmet>
+      <div className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <p className=" text-center text-4xl font-bold text-white">
+          {data?.category.category?.name}
+        </p>
+      </div>
+      {!loading && (
+        <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
+          <div className="flex justify-around max-w-3xl mx-auto">
+            {data?.allCategories.categories?.map((category) => (
+              <Link key={category.id} to={`/category/${category.slug}`}>
+                <div className="flex flex-col group items-center cursor-pointer">
+                  <div
+                    className=" w-16 h-16 rounded-full group-hover:bg-gray-100 bg-cover"
+                    style={{ backgroundImage: `url(${category.coverImg})` }}
+                  ></div>
+                  <span className="mt-3 text-sm text-center font-bold">
+                    {category.name}
+                  </span>
+                </div>
+              </Link>
+            ))}
+          </div>
+          <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+            {data?.category.restaurants?.map((restaurant) => (
+              <Restaurant
+                key={restaurant.id}
+                id={restaurant.id + ""}
+                coverImg={restaurant.coverImg}
+                name={restaurant.name}
+                categoryName={restaurant.category?.name}
+              />
+            ))}
+          </div>
+          <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className=" focus:outline-none font-medium text-2xl"
+              >
+                &larr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <span>
+              Page {page} of {data?.category.totalPages}
+            </span>
+            {page !== data?.category.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className=" focus:outline-none font-medium text-2xl"
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
