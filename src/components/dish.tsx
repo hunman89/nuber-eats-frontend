9c423f141d2c8c8c3c0c9710e7b0cfaf4631b393
hunman89 <hunman89@gmail.com>
2021-12-02 10:50:40 +0900

    add. dish options in client page

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -1,19 +1,40 @@
 import React from "react";
+import { restaurant_restaurant_restaurant_menu_options } from "../__generated__/restaurant";
 
 interface IDishProps {
   description: string;
   name: string;
   price: number;
+  isCustomer?: boolean;
+  options?: restaurant_restaurant_restaurant_menu_options[] | null;
 }
 
-export const Dish: React.FC<IDishProps> = ({ description, name, price }) => {
+export const Dish: React.FC<IDishProps> = ({
+  description,
+  name,
+  price,
+  isCustomer = false,
+  options,
+}) => {
+  console.log(options);
   return (
     <div className="px-8 py-4 border hover:border-gray-800 transition-all">
       <div className="mb-5">
         <h3 className="text-lg font-medium">{name}</h3>
         <h4 className="font-medium">{description}</h4>
       </div>
-      <span>{price}</span>
+      <span>${price}</span>
+      {isCustomer && options && options?.length !== 0 && (
+        <div>
+          <h5 className="mt-8 mb-3 font-medium">Dish Options:</h5>
+          {options?.map((option, index) => (
+            <span key={index} className="flex items-center">
+              <h6>{option.name}</h6>
+              <h6 className="ml-2 text-sm opacity-75">$({option.extra})</h6>
+            </span>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
