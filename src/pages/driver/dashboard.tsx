cca1b01c776354eb94b4222a50c6fc16b6f5f6b9
hunman89 <hunman89@gmail.com>
2021-12-05 11:45:30 +0900

    add. direction render

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
-import { mapSeries } from "cypress/types/bluebird";
+import { result } from "cypress/types/lodash";
 
 interface ICoords {
   lat: number;
   lng: number;
 }
 
+interface IDriverProps {
+  lat: number;
+  lng: number;
+  $hover?: any;
+}
+
+const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🛵</div>;
+
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
-  const [map, setMap] = useState<any>();
+  const [map, setMap] = useState<google.maps.Map>();
   const [maps, setMaps] = useState<any>();
   const onSucces = ({
     coords: { latitude, longitude },
@@ -26,13 +34,51 @@ export const Dashboard = () => {
   }, []);
   useEffect(() => {
     if (map && maps) {
-      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      //   const geocoder = new google.maps.Geocoder();
+      //   geocoder.geocode(
+      //     {
+      //       location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+      //     },
+      //     (results, status) => {
+      //       console.log(results, status);
+      //     }
+      //   );
     }
   }, [driverCoords.lat, driverCoords.lng]);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
-    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
     setMap(map);
     setMaps(maps);
+  };
+  const onGetRouteClick = () => {
+    if (map) {
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer({
+        polylineOptions: { strokeColor: "#000", strokeWeight: 3 },
+      });
+      directionsRenderer.setMap(map);
+      directionsService.route(
+        {
+          origin: {
+            location: new google.maps.LatLng(
+              driverCoords.lat,
+              driverCoords.lng
+            ),
+          },
+          destination: {
+            location: new google.maps.LatLng(
+              driverCoords.lat + 0.05,
+              driverCoords.lng + 0.05
+            ),
+          },
+          travelMode: google.maps.TravelMode.TRANSIT,
+        },
+        (result, status) => {
+          directionsRenderer.setDirections(result);
+        }
+      );
+    }
   };
   return (
     <div>
@@ -48,16 +94,10 @@ export const Dashboard = () => {
           bootstrapURLKeys={{ key: "AIzaSyBbAcnDyspvgSaWi8-OSe1JTdEg5OhEwmI" }}
           onGoogleApiLoaded={onApiLoaded}
         >
-          <div
-            // @ts-ignore
-            lat={driverCoords.lat}
-            lng={driverCoords.lng}
-            className="text-lg"
-          >
-            🛵
-          </div>
+          <Driver lat={driverCoords.lat} lng={driverCoords.lng}></Driver>
         </GoogleMapReact>
       </div>
+      <button onClick={onGetRouteClick}>Get Route</button>
     </div>
   );
 };
